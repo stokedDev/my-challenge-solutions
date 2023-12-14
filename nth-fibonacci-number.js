54bbab305bf78d8601b2ace4c3cf1a7080de5ade
Maurice Rogers <scsurfermaurice@gmail.com>
2023-12-13 21:11:27 -0800

    Update and rename nth-fib-num.js
    Rename nth-fib-num.js to nth-fibonacci-number.js and update JS with BigInt solution

---
+++ nth-fibonacci-number.js
@@ -0,0 +1,14 @@
+/* Write a function that returns the nth Fibonacci number. */
+
+/* My Solution */
+function fibonacci(n) {
+	const fibonacciArr = [0n,1n];
+	for(let i = 1; i <= n; i++){
+		fibonacciArr[1] = fibonacciArr[0] + fibonacciArr[1];
+		fibonacciArr[0] = fibonacciArr[0] + fibonacciArr[1];
+		if(i === n/2){
+		const nthFibonacci = `${fibonacciArr[0]}`.replace(`n`,'');
+		return nthFibonacci;
+		}
+	}
+}
